a4f0881506cf717b70cd73ed32ab4180694193cf
Mayur Kawle <mayur@futurewise.co.in>
2024-10-27 21:02:48 +0530

    Project config to use external tomcat

--- src/main/java/com/Hibernate/Java/Hibernate/JavaHibernateApplication.java
+++ src/main/java/com/Hibernate/Java/Hibernate/JavaHibernateApplication.java
@@ -2,12 +2,18 @@ package com.Hibernate.Java.Hibernate;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class JavaHibernateApplication {
+public class JavaHibernateApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JavaHibernateApplication.class, args);
 	}
 
+	@Override
+	public SpringApplicationBuilder configure(SpringApplicationBuilder builder){
+		return builder.sources(JavaHibernateApplication.class);
+	}
 }
